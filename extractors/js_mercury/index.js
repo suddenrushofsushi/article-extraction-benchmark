6101779caa18d2072d6e1dc9c62401fc8434accc
Craig Waterman <craigwaterman@gmail.com>
2022-06-06 19:17:42 -0400

    Adds mercury

--- extractors/js_mercury/index.js
+++ extractors/js_mercury/index.js
@@ -1,10 +1,12 @@
 const Mercury = require('@postlight/mercury-parser');
+const fs = require('fs');
 
 var args = process.argv.slice(2);
-console.log(args);
 var url = args[0];
+var html = fs.readFileSync(args[1], 'utf8');
+
 Mercury.parse(url, {
-  html:
-    '<html><body><article><h1>Thunder (mascot)</h1><p>Thunder is the stage name for the horse who is the official live animal mascot for the Denver Broncos</p></article></body></html>',
-}).then(result => console.log(result));
+  html: html,
+  contentType: 'text',
+}).then(result => console.log(result['content']));
 
--- extractors/run_js_mercury.py
+++ extractors/run_js_mercury.py
@@ -3,36 +3,46 @@ import gzip
 import json
 import os
 import subprocess
+import time
 from pathlib import Path
 from tempfile import mkstemp
 
 
 # built executable file
-CLI_PATH = Path('extractors/go_readability/go_readability_cli')
+CLI_PATH = Path('node extractors/js_mercury/index.js')
 
 
 def main():
+    gt = json.load(open('ground-truth.json'))
     output = {}
     for path in Path('html').glob('*.html.gz'):
         with gzip.open(path, 'rt', encoding='utf8') as f:
             html = f.read()
         item_id = path.stem.split('.')[0]
+        url = gt[item_id]["url"]
 
         # save html to temp file
-        temp_filepath = mkstemp()[1]
+        temp_filepath = f"/tmp/{item_id}_html"
         with open(temp_filepath, 'wt') as fw:
             fw.write(html)
 
-        # get extracted content from go-readadbility
-        result = subprocess.run([CLI_PATH, temp_filepath], stdout=subprocess.PIPE)
+        time.sleep(0.25)
+        command_str = f"{CLI_PATH} {url} {temp_filepath}"
+        print(command_str)
+
+        # get extracted content from mercury-parser
+        result = subprocess.run(command_str, stdout=subprocess.PIPE, shell=True)
 
         # destroy temp file
-        os.remove(temp_filepath)
+        # os.remove(temp_filepath)
 
-        output[item_id] = {'articleBody': result.stdout.decode('utf-8')}
-    (Path('output') / 'go_readability.json').write_text(
-        json.dumps(output, sort_keys=True, ensure_ascii=False, indent=4),
-        encoding='utf8')
+        # parse result
+        decoded = str(result.stdout)
+
+        output[item_id] = {'articleBody': decoded}
+        (Path('output') / 'js_mercury.json').write_text(
+            json.dumps(output, sort_keys=True, ensure_ascii=False, indent=4),
+            encoding='utf8')
 
 
 if __name__ == '__main__':
